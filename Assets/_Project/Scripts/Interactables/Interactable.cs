265fe5ae7f5b24fa65a48553f3edbda324c3e402
aaron-h6665 <aaron_huang1@brown.edu>
2026-07-12 16:33:18 -0400

    Add faint pulsing yellow glow to interactable objects in range of raycast; fix box colliders with interactable block layer and safe.

--- Assets/_Project/Scripts/Interactables/Interactable.cs
+++ Assets/_Project/Scripts/Interactables/Interactable.cs
@@ -1,9 +1,30 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public abstract class Interactable : MonoBehaviour
 {
     [SerializeField]
     protected string promptMessage;
+
+    [Header("Focus Highlight")]
+    [SerializeField] protected bool highlightOnFocus = true;
+    [SerializeField] protected Color highlightColor = new Color(1f, 0.82f, 0.18f, 1f);
+    [SerializeField] protected float highlightMinIntensity = 0.15f;
+    [SerializeField] protected float highlightMaxIntensity = 0.55f;
+    [SerializeField] protected float highlightPulseSpeed = 2.5f;
+    [SerializeField] protected Renderer[] highlightRenderers;
+
+    static readonly int EmissionColorId = Shader.PropertyToID("_EmissionColor");
+    readonly List<HighlightMaterialState> highlightMaterials = new List<HighlightMaterialState>();
+    bool highlightInitialized;
+    bool focused;
+
+    struct HighlightMaterialState
+    {
+        public Material material;
+        public Color originalEmissionColor;
+        public bool originallyEmissionEnabled;
+    }
 
     public virtual string GetPromptMessage()
     {
@@ -18,5 +39,122 @@ public abstract class Interactable : MonoBehaviour
     protected virtual void Interact(GameObject interactor)
     {
 
+    }
+
+    public void SetFocused(bool isFocused)
+    {
+        if (focused == isFocused)
+        {
+            return;
+        }
+
+        focused = isFocused;
+
+        if (!highlightOnFocus)
+        {
+            return;
+        }
+
+        if (focused)
+        {
+            EnsureHighlightMaterials();
+            ApplyFocusHighlight();
+        }
+        else
+        {
+            ClearFocusHighlight();
+        }
+    }
+
+    public void UpdateFocusHighlight()
+    {
+        if (!focused || !highlightOnFocus)
+        {
+            return;
+        }
+
+        ApplyFocusHighlight();
+    }
+
+    protected virtual void OnDisable()
+    {
+        if (focused)
+        {
+            SetFocused(false);
+        }
+    }
+
+    void EnsureHighlightMaterials()
+    {
+        if (highlightInitialized)
+        {
+            return;
+        }
+
+        highlightInitialized = true;
+        Renderer[] renderers = highlightRenderers != null && highlightRenderers.Length > 0
+            ? highlightRenderers
+            : GetComponentsInChildren<Renderer>(true);
+
+        foreach (Renderer targetRenderer in renderers)
+        {
+            if (targetRenderer == null)
+            {
+                continue;
+            }
+
+            Material[] materials = targetRenderer.materials;
+            foreach (Material material in materials)
+            {
+                if (material == null || !material.HasProperty(EmissionColorId))
+                {
+                    continue;
+                }
+
+                highlightMaterials.Add(new HighlightMaterialState
+                {
+                    material = material,
+                    originalEmissionColor = material.GetColor(EmissionColorId),
+                    originallyEmissionEnabled = material.IsKeywordEnabled("_EMISSION")
+                });
+            }
+        }
+    }
+
+    void ApplyFocusHighlight()
+    {
+        EnsureHighlightMaterials();
+
+        float pulse = Mathf.PingPong(Time.time * highlightPulseSpeed, 1f);
+        float intensity = Mathf.Lerp(highlightMinIntensity, highlightMaxIntensity, pulse);
+        Color emissionColor = highlightColor * Mathf.Max(0f, intensity);
+
+        foreach (HighlightMaterialState highlightMaterial in highlightMaterials)
+        {
+            if (highlightMaterial.material == null)
+            {
+                continue;
+            }
+
+            highlightMaterial.material.EnableKeyword("_EMISSION");
+            highlightMaterial.material.SetColor(EmissionColorId, emissionColor);
+        }
+    }
+
+    void ClearFocusHighlight()
+    {
+        foreach (HighlightMaterialState highlightMaterial in highlightMaterials)
+        {
+            if (highlightMaterial.material == null)
+            {
+                continue;
+            }
+
+            highlightMaterial.material.SetColor(EmissionColorId, highlightMaterial.originalEmissionColor);
+            if (!highlightMaterial.originallyEmissionEnabled)
+            {
+                highlightMaterial.material.DisableKeyword("_EMISSION");
+            }
+        }
     }
 }
--- Assets/_Project/Scripts/Player/PlayerInteract.cs
+++ Assets/_Project/Scripts/Player/PlayerInteract.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
+    const int MaxRaycastHits = 16;
+
     private Camera cam;
     [SerializeField]
     private float distance = 10f;
@@ -11,6 +15,9 @@ public class PlayerInteract : MonoBehaviour
     private LayerMask blockerMask;
     private PlayerUI playerUI;
     private InputManager inputManager;
+    private Interactable focusedInteractable;
+    private readonly RaycastHit[] hitBuffer = new RaycastHit[MaxRaycastHits];
+    private static readonly RaycastHitDistanceComparer hitDistanceComparer = new RaycastHitDistanceComparer();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,30 +32,108 @@ public class PlayerInteract : MonoBehaviour
     {
         if (ReplayManager.IsPlaybackActive())
         {
+            SetFocusedInteractable(null);
             return;
         }
 
         if (cam == null || playerUI == null || inputManager == null)
         {
+            SetFocusedInteractable(null);
             return;
         }
 
         playerUI.UpdateText(string.Empty);
 
-        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-        RaycastHit hitInfo;
         int raycastMask = mask.value | blockerMask.value;
-        if (Physics.Raycast(ray, out hitInfo, distance, raycastMask, QueryTriggerInteraction.Collide))
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        Interactable interactable = FindInteractableInView(ray, raycastMask);
+        if (interactable != null)
         {
-            Interactable interactable = hitInfo.collider.GetComponentInParent<Interactable>();
+            playerUI.UpdateText(interactable.GetPromptMessage());
+            if (inputManager.OnFoot.Interact.triggered)
+            {
+                interactable.BaseInteract(gameObject);
+            }
+        }
+
+        SetFocusedInteractable(interactable);
+        focusedInteractable?.UpdateFocusHighlight();
+    }
+
+    void OnDisable()
+    {
+        SetFocusedInteractable(null);
+    }
+
+    void SetFocusedInteractable(Interactable interactable)
+    {
+        if (focusedInteractable == interactable)
+        {
+            return;
+        }
+
+        if (focusedInteractable != null)
+        {
+            focusedInteractable.SetFocused(false);
+        }
+
+        focusedInteractable = interactable;
+
+        if (focusedInteractable != null)
+        {
+            focusedInteractable.SetFocused(true);
+        }
+    }
+
+    Interactable FindInteractableInView(Ray ray, int raycastMask)
+    {
+        int hitCount = Physics.RaycastNonAlloc(ray, hitBuffer, distance, raycastMask, QueryTriggerInteraction.Collide);
+        if (hitCount == hitBuffer.Length)
+        {
+            RaycastHit[] hits = Physics.RaycastAll(ray, distance, raycastMask, QueryTriggerInteraction.Collide);
+            Array.Sort(hits, hitDistanceComparer);
+            return FindInteractableInHits(hits, hits.Length);
+        }
+
+        Array.Sort(hitBuffer, 0, hitCount, hitDistanceComparer);
+        return FindInteractableInHits(hitBuffer, hitCount);
+    }
+
+    Interactable FindInteractableInHits(RaycastHit[] hits, int hitCount)
+    {
+        for (int i = 0; i < hitCount; i++)
+        {
+            Collider hitCollider = hits[i].collider;
+            if (hitCollider == null)
+            {
+                continue;
+            }
+
+            Interactable interactable = hitCollider.GetComponentInParent<Interactable>();
             if (interactable != null)
             {
-                playerUI.UpdateText(interactable.GetPromptMessage());
-                if (inputManager.OnFoot.Interact.triggered)
-                {
-                    interactable.BaseInteract(gameObject);
-                }
+                return interactable;
             }
+
+            if (IsInLayerMask(hitCollider.gameObject.layer, blockerMask))
+            {
+                return null;
+            }
+        }
+
+        return null;
+    }
+
+    bool IsInLayerMask(int layer, LayerMask layerMask)
+    {
+        return (layerMask.value & (1 << layer)) != 0;
+    }
+
+    class RaycastHitDistanceComparer : IComparer<RaycastHit>
+    {
+        public int Compare(RaycastHit x, RaycastHit y)
+        {
+            return x.distance.CompareTo(y.distance);
         }
     }
 }
